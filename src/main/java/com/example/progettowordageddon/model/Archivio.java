815d8bbf5321d954b02b318837f8ce93e62cf75f
Carlo <c.cesarano18@studenti.unisa.it>
2025-05-29 15:28:40 +0200

    Aggiunta una funzionalità di base alla classe Archivio

--- src/main/java/com/example/progettowordageddon/model/Archivio.java
+++ src/main/java/com/example/progettowordageddon/model/Archivio.java
@@ -1,4 +1,26 @@
 package com.example.progettowordageddon.model;
 
+import java.util.ArrayList;
+
 public class Archivio {
+    private final ArrayList<DocumentoTestuale> archivio = new ArrayList<>();
+
+    public void pulisci() {
+        archivio.clear();
+    }
+
+    public void aggiungi(DocumentoTestuale doc) {
+        archivio.add(doc);
+    }
+
+    public void rimuovi(String nome) {
+        archivio.removeIf(doc -> doc.getNome().equals(nome));
+    }
+
+    public DocumentoTestuale get(String nome) {
+        for (DocumentoTestuale doc : archivio)
+            if (doc.getNome().equals(nome))
+                return doc;
+        return null;
+    }
 }
