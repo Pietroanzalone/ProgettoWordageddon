2c621b71d7d577e95916a2bc469bf6880eee98e2
Carlo <c.cesarano18@studenti.unisa.it>
2025-06-09 15:13:48 +0200

    Fixato il problema delle domande uguali nei quiz + Visual update alla schermata "Domanda"

--- src/main/java/com/example/progettowordageddon/model/Domanda.java
+++ src/main/java/com/example/progettowordageddon/model/Domanda.java
@@ -28,7 +28,7 @@ public class Domanda implements Serializable {
     private final String risposta4;
 
     /** @brief Indice (0-3) della risposta corretta. */
-    private final int corretta;
+    private final int correttaIdx;
 
     /** @brief Indice (0-3) della risposta selezionata, o null se non risposto. */
     private Integer selezionata;
@@ -41,15 +41,15 @@ public class Domanda implements Serializable {
      * @param risposta2 Seconda risposta.
      * @param risposta3 Terza risposta.
      * @param risposta4 Quarta risposta.
-     * @param corretta Indice della risposta corretta (0-3).
+     * @param correttaIdx Indice della risposta corretta (0-3).
      */
-    public Domanda(String testoDomanda, String risposta1, String risposta2, String risposta3, String risposta4, int corretta) {
+    public Domanda(String testoDomanda, String risposta1, String risposta2, String risposta3, String risposta4, int correttaIdx) {
         this.testoDomanda = testoDomanda;
         this.risposta1 = risposta1;
         this.risposta2 = risposta2;
         this.risposta3 = risposta3;
         this.risposta4 = risposta4;
-        this.corretta = corretta;
+        this.correttaIdx = correttaIdx;
     }
 
     public String getTestoDomanda(){
@@ -70,6 +70,16 @@ public class Domanda implements Serializable {
 
     public String getRisposta4(){
         return risposta4;
+    }
+
+    public String getCorretta() {
+        return switch (correttaIdx) {
+            case 0 -> risposta1;
+            case 1 -> risposta2;
+            case 2 -> risposta3;
+            case 3 -> risposta4;
+            default -> null;
+        };
     }
 
     /**
@@ -93,16 +103,7 @@ public class Domanda implements Serializable {
      * @return true se la risposta selezionata corrisponde a quella corretta, false altrimenti.
      */
     public boolean isCorretta() {
-        return selezionata == corretta;
-    }
-
-    /**
-     * @brief Calcola l'hash della domanda basato su testo e risposta corretta.
-     * @return Hash code della domanda.
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(testoDomanda, corretta);
+        return selezionata == correttaIdx;
     }
 
     /**
@@ -112,20 +113,10 @@ public class Domanda implements Serializable {
      */
     @Override
     public boolean equals(Object obj) {
-        if (obj instanceof Domanda d)
-            return hashCode() == d.hashCode();
-        return false;
+        if (!(obj instanceof Domanda d))
+            return false;
+        return Objects.equals(testoDomanda, d.getTestoDomanda())
+            && Objects.equals(getCorretta(), d.getCorretta());
     }
 
-    /**
-     * @brief Stampa la domanda e le sue opzioni sulla console (debug/testing).
-     */
-    public void chiediDomanda() {
-        System.out.println(testoDomanda);
-        System.out.println("A: " + risposta1);
-        System.out.println("B: " + risposta2);
-        System.out.println("C: " + risposta3);
-        System.out.println("D: " + risposta4);
-        System.out.println("Corretta: " + corretta);
-    }
 }
